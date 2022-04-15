7b18568294ee00cc9212f7c9f9a3bdd2c4384551
Yalegama <nadunyalegama@.com>
2022-04-15 18:56:47 +0530

    server

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const mysql = require('mysql');
+const csv = require('fast-csv');
+
+// Import CSV Data to MySQL database
+importCsvData2MySQL('customers.csv');
+
+function importCsvData2MySQL(filename){
+	let stream = fs.createReadStream(filename);
+	let csvData = [];
+	let csvStream = csv
+		.parse()
+		.on("data", function (data) {
+			csvData.push(data);
+		})
+		.on("end", function () {
+			// -> Remove Header ROW
+			csvData.shift();
+			
+			// -> Create a connection to the database
+			const connection = mysql.createConnection({
+				host: 'localhost',
+				user: 'root',
+				password: '',
+				database: 'testdb'
+			});
+
+			// Open the MySQL connection
+			connection.connect((error) => {
+				if (error) {
+					console.error(error);
+				} else {
+					let query = 'INSERT INTO customer (id, address, name, age) VALUES ?';
+					connection.query(query, [csvData], (error, response) => {
+						console.log(error || response);
+					});
+				}
+			});
+		});
+
+	stream.pipe(csvStream);
+}
